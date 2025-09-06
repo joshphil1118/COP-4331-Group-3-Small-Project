410407bde3fe10159d6b0ef725fec288d3383a37
KA-RD <kevin.delgado3174@yahoo.com>
2025-09-05 23:02:14 -0400

    feat: added popout forms for login and register.
    cleaned up the screen by making a forms popout, including adding the necessary js file.

---
+++ js/action_page.js
@@ -0,0 +1,18 @@
+// const urlBase = 'http://138.197.74.112/js';
+
+function openForm() {
+  document.getElementById("login").style.display = "block";
+
+}
+
+function openForm2() {
+  document.getElementById("register").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("login").style.display = "none";
+} 
+
+function closeForm2() {
+  document.getElementById("register").style.display = "none";
+} 
